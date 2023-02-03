958127e5229dd56e30383dd8c16129b232aa1a31
Vitor Daniel <vitor036daniel@gmail.com>
2023-02-03 07:52:28 -0300

    add only download file route

--- main.py
+++ main.py
@@ -3,24 +3,34 @@ import pathlib
 import tempfile
 
 import docker
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Response, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 client = docker.from_env()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 pathlib.Path("./stickers").mkdir(parents=True, exist_ok=True)
 
 
 @app.post("/")
-async def convert(file: UploadFile):
+async def convert(file: UploadFile, response: Response):
     content = await file.read()
     file_hash = hashlib.sha256(content).hexdigest()
 
+    response.headers["X-File-Hash"] = file_hash
+
     if pathlib.Path(f"./stickers/{file_hash}.gif").exists():
-        with open(f"./stickers/{file_hash}.gif", "rb") as buffer:
-            return FileResponse(f"./stickers/{file_hash}.gif")
+        return FileResponse(f"./stickers/{file_hash}.gif")
 
     with tempfile.TemporaryDirectory() as file_dir:
         file_name = f"{file_dir}/sticker.tgs"
@@ -38,3 +48,8 @@ async def convert(file: UploadFile):
                 output.write(buffer.read())
 
             return FileResponse(f"./stickers/{file_hash}.gif")
+
+
+@app.get("/{file_hash}")
+async def get_sticker(file_hash: str):
+    return FileResponse(f"./stickers/{file_hash}.gif")
